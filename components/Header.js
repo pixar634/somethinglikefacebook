719d6bc400a73c8971a0a567d58f6756add3dd40
Soumava Paul <soumava.paul@themathcompany.com>
2021-05-25 02:49:33 +0530

    polishing header css

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
-import { 
+import {
     BellIcon,
     ChatIcon,
     ChevronDownIcon,
@@ -19,48 +19,53 @@ import HeaderIcon from './HeaderIcon'
 
 function Header() {
     return (
-        <div className="sticky top-0 z-50 flex bg-white items-centre p-2 lg:px-5 shadow-md ">
+        <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
             {/* left */}
-            <div className="flex items-centre">               
-                    {/* <SearchIcon className="h-6" />                     */}
-                     <Image src ="https://links.papareact.com/5me" 
-                     width={40} 
-                     height={40} 
-                     layout = 'fixed'/>
-                
-                <div className="flex ml-2 items-centre rounded-full bg-gray-100 p-2">
-                    <SearchIcon className="h-6 text-gray-400"/> 
-                    <input 
-                    className="hidden md:inline-flex  ml-2 items-center bg-transparent outline-none placeholder-gray-500" type="text" 
-                    placeholder="Search Facebook" />
+            <div className="flex items-center">
+                <Image
+                    src="https://links.papareact.com/5me"
+                    width="40"
+                    height="40"
+                    layout="fixed"
+                />
+                <div className="hidden md:inline-flex ml-2 items-center rounded-full bg-gray-100 p-2">
+                    <SearchIcon className="h-6 text-gray-600" />
+                    <input
+                        className="hidden lg:inline-flex ml-2 bg-transparent outline-none placeholder-gray-500 flex-shrink"
+                        placeholder="Search Facebook"
+                    />
                 </div>
             </div>
-            
             {/* Centre */}
-            <div  className="flex justify-center flex-grow ">
-                <div className="flex space-x-6 md:space-x-20">
-                <HeaderIcon Active Icon={HomeIcon}/> 
-                <HeaderIcon Icon={FlagIcon}/> 
-                <HeaderIcon Icon={PlayIcon}/> 
-                <HeaderIcon Icon={ShoppingCartIcon}/> 
-                <HeaderIcon Icon={UserGroupIcon}/> 
+            <div className="flex justify-center flex-grow">
+                <div className="flex space-x-6 md:space-x-2">
+                    <HeaderIcon Active Icon={HomeIcon} />
+                    <HeaderIcon Icon={FlagIcon} />
+                    <HeaderIcon Icon={PlayIcon} />
+                    <HeaderIcon Icon={ShoppingCartIcon} />
+                    <HeaderIcon Icon={UserGroupIcon} />
                 </div>
             </div>
-        
 
-            
+
+
             {/* Right */}
-            <div>
-                {/* Profile Pic */}
-                <p className="whitespace-nowrap font-semibold pr-3">@mrmonochrome</p>
-                <div>
+            <div className="flex items-center sm:space-x-2 justify-end">
 
-                </div>
+
+                <p className="hidden lg:inline-flex text-sm whitespace-nowrap font-semibold pr-3">
+                    @mrmonochrome
+                </p>
+                <ViewGridIcon className="icon" />
+                <ChatIcon className="icon" />
+                <BellIcon className="icon" />
+                <ChevronDownIcon className="icon" />
+
             </div>
 
         </div>
-        
-        
+
+
     )
 }
 
--- components/HeaderIcon.js
+++ components/HeaderIcon.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function HeaderIcon({Active, Icon}) {
     return (
-        <div className='flex items-centre cursor-pointer md:px-10 sm:h-14 md:hover:bg-gray-100 rounded-xl active:border-b-2 active:border-blue-500 group '>
+        <div className="flex items-center md:px-10 sm:h-14 cursor-pointer md:hover:bg-gray-100 rounded-xl group active:border-b-2 active:border-blue-500">
             <Icon className={`h-5 test-gray-500 text-centre sm:h-7 mx-auto group-hover:text-blue-500 sm
             ${Active  && 'text-blue-500'}`}/>
         </div>
