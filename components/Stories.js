5f4a00051ef196d3f27ea98cc526122ec70cd029
Soumava Paul <soumava@mdbrd.in>
2021-05-27 01:36:40 +0530

    just made the first story dynamic, harcoding didnt make sense

--- components/Stories.js
+++ components/Stories.js
@@ -1,35 +1,39 @@
 import React from 'react'
 import StoryCard from './StoryCard'
-const stories = [
-    {
-        name: "Soumava Paul",
-        src: "https://links.papareact.com/zof",
-        profile: "https://links.papareact.com/l4v",
-    },
-    {
-        name: "Elon Musk",
-        src: "https://links.papareact.com/4zn",
-        profile: "https://links.papareact.com/kxk",
-    },
-    {
-        name: "Jeff Bezoz",
-        src: "https://links.papareact.com/k2j",
-        profile: "https://links.papareact.com/f0p",
-    },
-    {
-        name: "Mark Zuckerberg",
-        src: "https://links.papareact.com/xql",
-        profile: "https://links.papareact.com/snf",
-    },
-    {
-        name: "Bill Gates",
-        src: "https://links.papareact.com/4u4",
-        profile: "https://links.papareact.com/zvy",
-    },
-];
+import { useSession } from "next-auth/client"
+
 
 
 function Stories() {
+    const [session] = useSession()
+    const stories = [
+        {
+            name: "Soumava Paul",
+            src: "https://links.papareact.com/4zn",
+            profile: session.user.image,
+        },
+        {
+            name: "Elon Musk",
+            src: "https://links.papareact.com/4zn",
+            profile: "https://links.papareact.com/kxk",
+        },
+        {
+            name: "Jeff Bezoz",
+            src: "https://links.papareact.com/k2j",
+            profile: "https://links.papareact.com/f0p",
+        },
+        {
+            name: "Mark Zuckerberg",
+            src: "https://links.papareact.com/xql",
+            profile: "https://links.papareact.com/snf",
+        },
+        {
+            name: "Bill Gates",
+            src: "https://links.papareact.com/4u4",
+            profile: "https://links.papareact.com/zvy",
+        },
+    ];
+
     return (
         <div className="flex justify-centre space-x-3 mx-auto">
             {/* <h1>testing</h1> */}
